065f884d9a992923f15cffcbd985af32b46b40f8
201244027 <show654@naver.com>
2016-09-29 21:17:13 +0900

    report1.java °úÁ¦ 1¹ø

---
+++ report1.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class report1 {
+        void print(){
+	Scanner s = new Scanner(System.in);
+	System.out.print("태어난 년도를 입력하세요.");
+	int birth_year = s.nextInt();
+	int age = 2014 - birth_year + 1;
+	if (age < 20) {
+	System.out.print("미성년자 입니다.");
+	  }
+	  else {
+	  System.out.print("미성년자가 아닙니다.");
+	      }
+	       }
+        public static void main(String[] args) {
+        new report1().print();
+        }
+	}
+
