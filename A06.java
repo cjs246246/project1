32b44a2ac28a122527992466e009fe96a5e1230f
201244027 <show654@naver.com>
2016-10-01 11:05:11 +0900

    A06 수업자료 등록

--- A06.java
+++ A06.java
@@ -1,51 +1,61 @@
 import java.util.*;
 class A06 {
-        public static void main(String[] args) {
-        int kor=0;
-	int eng=0;
-	int math=0;
-	int total=0;
-	char name;
-	double avg=0;
+	public static void main(String[] args) {
+
+
 	Scanner s = new Scanner(System.in);
 	System.out.print("이름 : ");
-	do{	
-	System.out.print("국어 점수를 입력하세요 ");
-	kor = s.nextInt();
-	} while(kor ==0);
-	do{														System.out.print("영어 점수를 입력하세요 ");
- 	eng = s.nextInt();
-	} while(eng == 0);
-	do{  System.out.print("수학 점수를 입력하세요 ");
-	math = s.nextInt();
-	} while(math == 0);
-	total = kor + eng + math;
-	avg = 1.0*total/3;
-	System.out.println(" 님은 \n평균은 "  );
+	String name = s.nextLine();
+	System.out.print("국어점수 입력 : ");
+	int kor = s.nextInt();
+	System.out.print("수학점수 입력 : ");
+	int math = s.nextInt();
+	System.out.print("영어점수 입력 : ");
+	int eng = s.nextInt();
+	double avg = (kor + math + eng)/3;
+	System.out.print(name + "님은\n평균은 " + avg + "이고\n");
 	if(avg >= 95) {
-	System.out.print("학점은 A+ 입니다 .\n");
-	 }
-	 else {}
-	     if(avg >= 90) {
-	     System.out.print("학점은 A 입니다.\n");
-	        }
-	else {}
-	if(avg >= 85) {
-	System.out.print("학점은 B+ 입니다.\n");
-		}	
-		else {}
-			       if(avg >= 80) {
-			         System.out.print("학점은 B 입니다.\n");
-			        }
-			else {}
-			       if(avg >= 75) {
-			        System.out.print("학점은 C+ 입니다.\n");
-			        }
-			else {}
-			      if(avg >= 70) {
-			       System.out.print("학점은 C 입니다.\n");
-			        }
-				else {}
+	System.out.print("학점은 A+ 입니다.");
+	}
 
-				     }
-				 }
+	else if(avg < 95 && avg >= 90) {
+	System.out.print("학점은 A 입니다.");
+	}
+
+	else if(avg < 90 && avg >= 85) {	
+	System.out.print("학점은 B+ 입니다.");
+	}
+
+	else if(avg < 85 && avg >= 80) {
+	System.out.print("학점은 B 입니다.");
+	}
+
+	else if(avg < 80 && avg >= 75) {
+	System.out.print("학점은 C+ 입니다.");
+	}
+
+	else if(avg < 75 && avg >= 70) {
+	System.out.print("학점은 C 입니다.");
+	}
+
+	else if(avg < 70 && avg >= 65) {
+	System.out.print("학점은 D+ 입니다.");
+	}
+
+	else if(avg < 65 && avg >= 60) {
+	System.out.print("학점은 D 입니다.");
+	}
+
+	else if(avg < 60 && avg >= 0) {
+	System.out.print("학점은 F 입니다.");
+	}
+
+	else {
+	System.out.print("잘못된 학점입니다.");
+
+	}
+
+	}
+
+	}
+
